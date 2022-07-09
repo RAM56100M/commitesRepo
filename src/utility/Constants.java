ae86bdca29442bd1e14bdaa421a937ea0c2a8c4a
RAM <srpatil9944@gmail.com>
2022-07-10 00:10:21 +0530

    commited

---
+++ src/utility/Constants.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class Constants {
+	public static final String URL = "https://demoqa.com/automation-practice-form";
+    public static final String Path_TestData = "E:\\Projects\\src\\testData\\";
+    public static final String File_TestData = "TestData.xls";
+}
+

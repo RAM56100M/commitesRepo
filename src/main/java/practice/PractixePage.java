1125f3b713f333a4b05d199308b8e252ab034636
w3schools-test <test@w3schools.com>
2022-07-30 00:18:59 +0530

    Star Prrinting

---
+++ src/main/java/practice/PractixePage.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class PractixePage {
+
+	public static void main(String[] args) {
+		System.out.println("RAM MARSHIVANE");
+		for (int i=0;i<=5;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+				{for (int k=0;k<=i;k++) {
+					System.out.print("*");
+				}
+			
+			System.out.println();
+		}
+	}
+
+}}
